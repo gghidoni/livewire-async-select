3b3e288f1d58c9ed1f95ada82cda682e53aef3ca
Serge Bénard <serge.benard@gmail.com>
2025-11-16 13:39:43 -0500

    Add French local (#6)

---
+++ resources/lang/fr/async-select.php
@@ -0,0 +1,18 @@
+<?php
+
+return [
+    'select_option' => 'Sélectionnez une option',
+    'search' => 'Rechercher...',
+    'loading' => 'Chargement en cours...',
+    'loading_more' => 'Charger plus d\'items...',
+    'searching' => 'Recherche en cours...',
+    'no_results' => 'Aucun résultat trouvé',
+    'no_results_for' => 'Aucun résultat trouvé pour',
+    'no_options' => 'Aucune option disponible',
+    'clear' => 'Effacer',
+    'remove' => 'Supprimer',
+    'selected' => 'Choisie',
+    'scroll_more' => 'Défiler pour plus de résultats...',
+    'type_to_search' => 'Saisissez les termes de recherche...',
+    'min_chars' => 'Saisissez au moins :min charactères pour effectuer une recherche',
+];
